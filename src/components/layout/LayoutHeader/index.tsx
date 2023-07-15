d862fd8772a0fa81b0dc5a886ca7cc91e5fc8552
matsu <donguri47@gmail.com>
2023-07-15 13:10:48 +0900

    style: デフォルトレイアウトのヘッダーの色を変更

--- src/components/layout/LayoutHeader/index.tsx
+++ src/components/layout/LayoutHeader/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const LayoutHeader = () => {
   return (
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <header className="navbar navbar-dark sticky-top bg-info flex-md-nowrap p-0 shadow">
       <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="./">
         ロゴ
       </a>
